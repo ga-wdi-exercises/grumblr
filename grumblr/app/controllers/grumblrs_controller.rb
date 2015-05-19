95d1c0dbcdda66706248e9f1aba769b8319244e0
Toby <toby.schaeffer@gmail.com>
2015-05-19 17:43:32 -0400

    initial commit.  building out files

---
+++ grumblr/app/controllers/grumblrs_controller.rb
@@ -0,0 +1,37 @@
+class GrumblrsController < ApplicationController
+  def index
+    @grumblrs= Grumblr.all
+    render statsus: 200, json @grumblrs.to_json
+  end
+
+  def show
+    @grumblr = Grumblr.find(params[:id])
+    render status: 200, json @grumblr.to_json
+  end
+
+  def create
+    @grumblr = Grumblr.new(grumblr_params)
+    if @grumblr.save
+      render json @grumblr.to_json, status: 200
+    end
+  end
+
+  def update
+    @grumblr = Grumblr.find(params[:id])
+    if @grumblr.update(grumblr_params)
+      render json: @grumblr.to_json, status: 200
+    end
+  end
+
+  def destroy 
+    @grumblr = Grumblr.find(params[:id])
+    if @grumblr.destroy
+      render json: @grumblr.to_json, status 200
+    end
+  end
+
+  private
+  def grumblr_params
+    params.require(:grumblr).permit(:body, :author)
+  end
+end
