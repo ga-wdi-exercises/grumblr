42b85dc9108dccdafb48f10e562419dbac5d43e9
Hilary Masland <hilarymasland@gmail.com>
2016-03-31 06:51:11 -0400

    controller functionality

--- show.controler.js
+++ show.controler.js
@@ -2,6 +2,12 @@
 (function(){
   angular
   .module()
-  .contorller()
-
+  .contorller("GrumblesShowController", [
+    "GrumbleFactory"
+    "$stateParams",
+    GrumbleShowControllerFunction
+  ]);
+function GrumbleShowControllerFunction( GrumbleFactory, $stateParams){
+  this.grumble = GrumbleFactory.get({id: $stateParams.id});
+}
 })();
